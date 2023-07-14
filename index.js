24f00401aace8ee316466803178fdbd23df27c74
Peeyush Singh <65058132+peeyush-es@users.noreply.github.com>
2023-07-14 17:04:05 +0530

    for return reponse

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 
+app.get('/api', (req, res) => {
+  res.json('{}');
+
+   //#res.sendStatus(200)
+});
+
 app.post('/api', (req, res) => {
   const requestBody = req.body;
 
   // Do something with the JSON data
-  console.log(requestBody);
+  //console.log(requestBody);
 
-  res.sendStatus(200);
+  //#res.sendStatus(200);
+    res.json(requestBody);
+
 });
 
 app.listen(port, () => {
